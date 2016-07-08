6a0f70dc02f1b1d60d6a4d8019e27e77d8721de7
Todd Tyree <tatyree@gmail.com>
2016-07-08 10:32:09 +0100

    Add govuk templates (#4)
    * Style with gov toolkit, use high_voltage for start
    
    Also, move copy out to I18N files
    
    * Remove coverage
    
    * Do not stage coverage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,6 @@ Rails.application.configure do
   config.consider_all_requests_local = true
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
-
     config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => 'public, max-age=172800'
@@ -13,18 +12,12 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
     config.cache_store = :null_store
   end
-
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
-
   config.active_support.deprecation = :log
-
   config.active_record.migration_error = :page_load
-
   config.assets.debug = true
-
   config.assets.quiet = true
-
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_view.raise_on_missing_translations = true
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,16 +5,12 @@ Rails.application.configure do
   config.public_file_server.headers = {
     'Cache-Control' => 'public, max-age=3600'
   }
-
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-
   config.action_dispatch.show_exceptions = false
-
   config.action_controller.allow_forgery_protection = false
   config.action_mailer.perform_caching = false
-
   config.action_mailer.delivery_method = :test
-
   config.active_support.deprecation = :stderr
+  config.action_view.raise_on_missing_translations = true
 end
