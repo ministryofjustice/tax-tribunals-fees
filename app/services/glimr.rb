0f060562e56f4673b7567c3afcc5f3e7b4647f79
Todd Tyree <tatyree@gmail.com>
2016-07-12 13:40:29 +0100

    Ensure all the included api libs are covered
    This should be the case as they have been ported over as required.
    However, explicit `requires` are preferred and ensure that simplecov
    (and the devs) do not miss anything.

--- app/services/glimr.rb
+++ app/services/glimr.rb
@@ -1,7 +1,11 @@
 require 'glimr/api'
 require 'glimr/requests/base'
 require 'glimr/requests/case_fees'
+require 'glimr/requests/fee_paid'
 require 'glimr/responses/api_error'
+require 'glimr/responses/case_fees'
+require 'glimr/responses/case_not_found'
+require 'glimr/responses/fee_payment'
 
 module Glimr
 end
