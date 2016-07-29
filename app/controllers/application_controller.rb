321df6883e73bd4527845951d66cc898fb9453a2
Todd Tyree <tatyree@gmail.com>
2016-07-29 13:43:11 +0100

    Continually check that glimr is available
    Previously, we were only checking this at the start of the run. This did
    not account for the possibility that glimr could go away at any point in
    the 'transaction'. The user would not have been notified until they
    attempted to complete a payment.  This will stop them as move between
    'states'/pages.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
+  rescue_from Glimr::Api::Unavailable, with: :service_not_available
   protect_from_forgery with: :exception
+
+  before_action :glimr_available
+
+  private
+
+  def service_not_available
+    @glimr_available = false
+    render 'pages/start'
+  end
+
+  def glimr_available
+    @glimr_available ||= Glimr.available?
+  end
 end
