cd32df4f407b701b1b2b7939865ea1ebafb5b8be
Todd Tyree <tatyree@gmail.com>
2016-08-01 16:04:50 +0100

    Store all the settings in the same place

--- config/initializers/govpay_details.rb
+++ config/initializers/govpay_details.rb
@@ -1,2 +1,3 @@
 Rails.configuration.govpay_api_url = ENV['GOVUK_PAY_API_URL']
 Rails.configuration.govpay_api_key = ENV['GOVUK_PAY_API_KEY']
+Rails.application.routes.default_url_options[:protocol] = 'https'
