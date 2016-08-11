05d015cad1c6cb0039b3328dd1055991311f7aa4
Todd Tyree <tatyree@gmail.com>
2016-08-11 13:35:10 +0100

    WIP: Replace HTTParty

--- app/api_clients/glimr/api.rb
+++ app/api_clients/glimr/api.rb
@@ -2,23 +2,15 @@ module Glimr
   class Api
     class Unavailable < StandardError; end
 
-    include HTTParty
-
-    base_uri Rails.configuration.glimr_api_url
-    default_timeout 8 # seconds
-    format :json
-    headers Accept: 'application/json'
-
     def post(endpoint, body)
-      post_handler(endpoint, body)
+      RestClient.post(
+        "#{Rails.configuration.glimr_api_url}#{endpoint}",
+        body,
+        content_type: :json,
+        accept: :json
+      )
     rescue SocketError, Timeout::Error => e
       raise Glimr::Api::Unavailable, e
-    end
-
-    private
-
-    def post_handler(endpoint, body)
-      self.class.post(endpoint, body: body)
     end
   end
 end
