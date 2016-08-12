a8e9d276a90c419df2452d2f84bc7d753982b1d6
David Salgado <david.salgado@digital.justice.gov.uk>
2016-08-12 14:55:44 +0100

    fix rubocop's complaints

--- spec/models/case_request_spec.rb
+++ spec/models/case_request_spec.rb
@@ -6,31 +6,37 @@ RSpec.describe CaseRequest do
 
   subject(:case_request) { described_class.new(case_reference, confirmation_code) }
 
-  let(:fee_liability) { double(
-    description:  "Fee liability",
-    amount:       10000,
-    glimr_id:     12345
-  ) }
+  let(:fee_liability) {
+    instance_double(
+      Glimr::Responses::CaseFees::FeeLiability,
+      description:  "Fee liability",
+      amount:       10_000,
+      glimr_id:     '12345'
+    )
+  }
 
   let(:fee_liabilities) { [fee_liability] }
 
-  let(:glimr_case_request) { double(
-    title:            "Glimr Case Request",
-    fee_liabilities:  fee_liabilities,
-    error?:           false
-  ) }
+  let(:glimr_case_request) {
+    instance_double(
+      Glimr::Responses::CaseFees,
+      title:            "Glimr Case Request",
+      fee_liabilities:  fee_liabilities,
+      error?:           false
+    )
+  }
 
-  let(:fee) { double(update_attributes: nil) }
+  let(:fee) { instance_double(Fee, update_attributes: nil) }
 
   describe '#all_fees_paid?' do
     it "knows all fees are paid" do
-      case_request.fees << double(paid?: true)
+      case_request.fees << instance_double(Fee, paid?: true)
       expect(case_request.all_fees_paid?).to be_truthy
     end
 
     it "knows not all fees are paid" do
-      case_request.fees << double(paid?: true)
-      case_request.fees << double(paid?: false)
+      case_request.fees << instance_double(Fee, paid?: true)
+      case_request.fees << instance_double(Fee, paid?: false)
       expect(case_request.all_fees_paid?).to be_falsey
     end
   end
@@ -47,13 +53,13 @@ RSpec.describe CaseRequest do
   end
 
   describe '#process!' do
-
     context "when case does not exist in glimr" do
-      let(:glimr_case_request) { double(
-        title:            "Glimr Case Request",
-        fee_liabilities:  fee_liabilities,
-        error?:           true
-      ) }
+      let(:glimr_case_request) {
+        instance_double(
+          Glimr::Responses::CaseNotFound,
+          error?: true
+        )
+      }
 
       before do
         allow(Fee).to receive(:create).and_return(fee)
@@ -62,7 +68,7 @@ RSpec.describe CaseRequest do
 
       it "adds case not found error" do
         case_request.process!
-        expect(case_request).to_not be_valid
+        expect(case_request).not_to be_valid
         expect(case_request.errors.to_a).to eq(["Could not find a case on our systems with these details."])
       end
     end
@@ -72,6 +78,16 @@ RSpec.describe CaseRequest do
         allow(Fee).to receive(:create).and_return(fee)
         allow(Glimr).to receive(:find_case).and_return(glimr_case_request)
       end
+
+      let(:fee_params) {
+        {
+          case_reference: case_reference,
+          case_title: "Glimr Case Request",
+          description: "Fee liability",
+          amount: 10_000,
+          glimr_id: '12345'
+        }
+      }
 
       it "is valid" do
         case_request.process!
@@ -89,21 +105,12 @@ RSpec.describe CaseRequest do
       end
 
       it "creates fee with correct parameters" do
-        expect(Fee).to receive(:create).with(
-          case_reference:  case_reference,
-          case_title:      "Glimr Case Request",
-          description:     "Fee liability",
-          amount:          10000,
-          glimr_id:        12345
-        )
-
+        expect(Fee).to receive(:create).with(fee_params)
         case_request.process!
       end
 
       it "stores the confirmation_code in the fee" do
-        expect(fee).to receive(:update_attributes)
-          .exactly(:once)
-          .with(confirmation_code: 'confcode')
+        expect(fee).to receive(:update_attributes).exactly(:once).with(confirmation_code: 'confcode')
         case_request.process!
       end
 
@@ -114,5 +121,4 @@ RSpec.describe CaseRequest do
       end
     end
   end
-
 end
