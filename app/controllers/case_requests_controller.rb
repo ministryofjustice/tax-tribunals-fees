86a0d6b6d7ab26092b657e19cd373d6420f72b88
David Salgado <david.salgado@digital.justice.gov.uk>
2016-08-12 14:24:15 +0100

    case reference & conf. code as keyword arguments
    Instead of passing a hash to CaseRequest.new, explicitly provide
    case_reference and confirmation_code

--- app/controllers/case_requests_controller.rb
+++ app/controllers/case_requests_controller.rb
@@ -1,13 +1,11 @@
 class CaseRequestsController < ApplicationController
   def new
-    @case_request = CaseRequest.new(
-      case_reference:     nil,
-      confirmation_code:  nil
-    )
+    @case_request = CaseRequest.new(nil, nil)
   end
 
   def create
-    @case_request = CaseRequest.new(case_request_params)
+    case_reference, confirmation_code = case_request_params
+    @case_request = CaseRequest.new(case_reference, confirmation_code)
 
     if @case_request.valid?
       @case_request.process!
@@ -20,7 +18,7 @@ class CaseRequestsController < ApplicationController
   private
 
   def case_request_params
-    params.require(:case_request).
-      permit(:case_reference, :confirmation_code).to_h
+    p = params.require(:case_request).permit(:case_reference, :confirmation_code).to_h
+    [ p.fetch(:case_reference), p.fetch(:confirmation_code) ]
   end
 end
