514d6cf88f50dfda14f720a8fee55693c17e2b82
David Salgado <david.salgado@digital.justice.gov.uk>
2016-10-27 14:38:47 +0100

    remove database cleaner
    It shouldn't be necessary, according to this;
    http://anti-pattern.com/transactional-fixtures-in-rails
    ...and it might be contributing to postgres
    deadlock problems with CircleCI

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,18 +14,12 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include ActiveSupport::Testing::TimeHelpers
 
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
   config.before(:all) do
     Excon.defaults[:mock] = true
   end
 
   config.before(:each) do
     I18n.locale = I18n.default_locale
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.start
     Excon.stub(
       { host: 'glimr-test.dsd.io', path: '/glimravailable' },
       { status: 200, body: { glimrAvailable: 'yes' }.to_json }
@@ -33,7 +27,6 @@ RSpec.configure do |config|
   end
 
   config.after(:each) do
-    DatabaseCleaner.clean
     Excon.stubs.clear
   end
 end
