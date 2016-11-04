30bdea257cb30134460d144be7e4bb59d0d581f8
David Salgado <david.salgado@digital.justice.gov.uk>
2016-11-04 17:13:49 +0000

    don't use ENV.fetch because it breaks jenkins
    When jenkins builds the docker container, it
    doesn't have a value for the environment variable

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ Rails.application.initialize!
 # Gov UK pay will redirect back to this host. This env var must be set
 # in production but is not required in dev & test
 Rails.application.routes.default_url_options[:host] =
-  ENV.fetch('GOV_UK_REDIRECT_HOST')
+  ENV['GOV_UK_REDIRECT_HOST']
