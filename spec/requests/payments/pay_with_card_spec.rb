4bdc337d9381b6fc251475dd893336c2a4588eb9
Todd Tyree <tatyree@gmail.com>
2016-12-07 13:23:12 +0000

    Stop failure due to unstubbed GLiMR call
    I missed the before block when I was setting this up.  It was working on
    the MoJ network because GLiMR was responding as expected.

--- spec/requests/payments/pay_with_card_spec.rb
+++ spec/requests/payments/pay_with_card_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe 'pay by card spec', type: :request do
   let(:api_available) { instance_double(GlimrApiClient::Available, available?: true) }
 
+  before do
+    allow(GlimrApiClient::Available).to receive(:call).and_return(api_available)
+  end
+
   context 'happy path' do
     specify {
       put '/payment_methods/abc123', params: { payment_method: 'card' }
