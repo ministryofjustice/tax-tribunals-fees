60b260b3cdc4d8915cabbd1f8a254165c990e685
Todd Tyree <tatyree@gmail.com>
2016-12-09 12:27:53 +0000

    Separate out help with fees concerns further
    It was using the payments layout, which is now just used to show the
    payment results pages.  Plus neaten up the html.

--- app/controllers/help_with_fees_controller.rb
+++ app/controllers/help_with_fees_controller.rb
@@ -1,11 +1,19 @@
 class HelpWithFeesController < ApplicationController
-  layout 'payments'
-
   def show
     @fee = Fee.find(params[:id])
   end
 
   def update
     @fee = Fee.find(params[:id])
+    if @fee
+      @fee.update_column(:help_with_fees_reference, help_with_fees_params)
+    end
+    redirect_to payment_url(@fee)
+  end
+
+  private
+
+  def help_with_fees_params
+    params.require(:help_with_fees_reference)
   end
 end
