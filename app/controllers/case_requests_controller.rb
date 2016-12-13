d211f252885bc4bd8f9a3fd2051c7b8842a77190
Todd Tyree <tatyree@gmail.com>
2016-12-13 11:03:53 +0000

    Set up mime response blocks
    Initial step in getting it to respond to JSON requests.

--- app/controllers/case_requests_controller.rb
+++ app/controllers/case_requests_controller.rb
@@ -7,21 +7,21 @@ class CaseRequestsController < ApplicationController
     case_reference, confirmation_code = case_request_params
     @case_request = CaseRequest.new(case_reference, confirmation_code)
 
-    if @case_request.valid?
-      @case_request.process!
-      render 'show'
-    else
-      render 'new'
+    respond_to do |format|
+      if @case_request.valid?
+        @case_request.process!
+        format.html { render 'show' }
+      else
+        format.html { render 'new' }
+      end
     end
   end
 
   private
 
   def case_request_params
-    p = params.
-        require(:case_request).
-        permit(:case_reference, :confirmation_code).
-        to_h
+    p = params.require(:case_request).
+        permit(:case_reference, :confirmation_code).to_h
     [p.fetch(:case_reference), p.fetch(:confirmation_code)]
   end
 end
