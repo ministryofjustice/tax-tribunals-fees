6e15341b0d2b0449e3ce883964e141dbda06ccca
Todd Tyree <tatyree@gmail.com>
2016-12-14 13:28:52 +0000

    Clear the database between runs
    This got overlooked some time ago, so records were hangning around
    between test runs.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,7 +12,7 @@ require 'webmock/rspec'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 
