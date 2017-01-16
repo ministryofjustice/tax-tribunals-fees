827a381212919ab11a71e204c020b902ea76a654
David Salgado <david.salgado@digital.justice.gov.uk>
2017-01-16 16:54:58 +0000

    add a workaround for staging error
    In the staging environment, the Glimr API Client
    'InvalidCaseNumber' exception is not triggering
    the controller 'rescue_from'. In development, it
    works fine.
    This is a quick hack to workaround this issue, 
    until we can come up with a proper fix.

--- app/models/case_request.rb
+++ app/models/case_request.rb
@@ -32,10 +32,40 @@ class CaseRequest < ApplicationRecord
   end
 
   def glimr_case_request
+    # This should raise a GlimrApiClient::Case::InvalidCaseNumber
+    # exception if the case is not found. But, in the staging (but
+    # not dev) environment, that exception is being swallowed, and
+    # we end up with a 'case' like this;
+    #
+    #  <GlimrApiClient::Case:0x007f94ecfc5750
+    #    @body=
+    #     "{\"message\":\"Invalid CaseNumber/CaseConfirmationCode
+    #     combination xxx / YYY\",\"glimrerrorcode\":213}",
+    #    @case_reference="xxx",
+    #    @confirmation_code="yyy">
+    #
+    # It's only when we try and call a method on this object that we
+    # get a KeyError. So, in the staging environment, the user sees
+    # a low-level application error when they search for a case with
+    # incorrect reference/confirmation_code
+    #
+    # The call to fees and the rescue below are a nasty hack to
+    # workaround this issue.  TODO: fix this properly, possibly by
+    # adding a 'has_errors?' method to the Case class, or by finding
+    # and fixing the underlying issue which is preventing the exception
+    # from bubbling up properly.
+
     @glimr_case_request ||= GlimrApiClient::Case.find(
       case_reference,
       confirmation_code
     )
+
+    # If we're looking at a failed find, this will raise a KeyError
+    @glimr_case_request.fees
+
+    @glimr_case_request
+  rescue KeyError
+    raise GlimrApiClient::Case::InvalidCaseNumber
   end
 
   delegate :fees, :title, to: :glimr_case_request
