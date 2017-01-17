00d966724b8a6eb3426956c163cbb5ef065df32f
David Salgado <david.salgado@digital.justice.gov.uk>
2017-01-17 12:02:29 +0000

    remove unused config setters

--- config/initializers/govpay_details.rb
+++ config/initializers/govpay_details.rb
@@ -1,3 +1,1 @@
-Rails.configuration.govpay_api_url = ENV['GOVUK_PAY_API_URL']
-Rails.configuration.govpay_api_key = ENV['GOVUK_PAY_API_KEY']
 Rails.application.routes.default_url_options[:protocol] = 'https'
