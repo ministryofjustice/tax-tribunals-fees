aeecb7e9e8d2426a44bf86153a1df3f570d12f33
David Salgado <david.salgado@digital.justice.gov.uk>
2017-03-24 11:18:53 +0000

    rename /healthcheck.json to /status.json
    "healthcheck" has a specific meaning for the
    platforms team (should this endpoint receive
    traffic from the load-balancer), so we should be
    using /status.json for our system status endpoints
    
    NB: There is a failing spec on the master branch
    of the fees app. This commit does not address that.
    
    https://www.pivotaltracker.com/story/show/142380681

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,9 +1,9 @@
-class HealthcheckController < ApplicationController
+class StatusController < ApplicationController
   rescue_from GlimrApiClient::Unavailable, with: :index
 
   respond_to :json
 
   def index
-    respond_with(Healthcheck.check.to_json)
+    respond_with(Status.check.to_json)
   end
 end
